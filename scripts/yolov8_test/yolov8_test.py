044a1419c8cbb2ed1ee183eb77f2cde1dd3b6503
recipelab <recipelab17@gmail.com>
2024-10-23 17:35:58 +0900

    add
    1. log 활성/비활성 추가

--- scripts/yolov8_test/yolov8_test.py
+++ scripts/yolov8_test/yolov8_test.py
@@ -10,8 +10,10 @@ from ultralytics import YOLO
 
 import argparse
 
+YOLO_LOG = True
 detection_model = YOLO("yolov8m.pt")
 segmentation_model = YOLO("yolov8m-seg.pt")
+
 rospy.init_node("yolov8")
 
 det_image_pub = rospy.Publisher("/ultralytics/detection/image", Image, queue_size=1)
@@ -34,12 +36,12 @@ def callback(data):
     """Callback function to process image and publish annotated images."""
     array = ros_numpy.numpify(data)
     if det_image_pub.get_num_connections():
-        det_result = detection_model(array)
+        det_result = detection_model(array, verbose=YOLO_LOG)
         det_annotated = det_result[0].plot(show=False)
         det_image_pub.publish(ros_numpy.msgify(Image, det_annotated, encoding="rgb8"))
 
     if seg_image_pub.get_num_connections():
-        seg_result = segmentation_model(array)
+        seg_result = segmentation_model(array, verbose=YOLO_LOG)
         seg_annotated = seg_result[0].plot(show=False)
         seg_image_pub.publish(ros_numpy.msgify(Image, seg_annotated, encoding="rgb8"))
 
